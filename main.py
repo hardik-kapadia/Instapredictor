ada4d5ad731a86a540c2b525a93ce7ff493de404
Hardik Kapadia <thecoderenroute@gmail.com>
2023-03-30 17:28:35 +0530

    minor

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ def read_names_from_files(filename: str = "instagram handle list.txt"):
 
     names = set()
 
+    count = 0
+
     with open(filename, "r") as f:
 
         for line in f:
+            if count >= 10:
+                break
+
+            count += 1
             names.add(line.strip())
 
     return names
@@ -16,9 +22,9 @@ def read_names_from_files(filename: str = "instagram handle list.txt"):
 if __name__ == "__main__":
     igd = InstagramData()
 
-    # profiles = read_names_from_files()
-    
-    profiles = ['shikhatalsania','zomato']
+    profiles = read_names_from_files()
+
+    # profiles = ['shikhatalsania','zomato']
 
     non_existant = []
     errors = []
@@ -28,4 +34,4 @@ if __name__ == "__main__":
             igd.download_ig_profile(profile)
         except ValueError as e:
             errors.append(profile)
-            print(f' getting error for profile {profile} -> {e}')
+            print(f" getting error for profile {profile} -> {e}")
